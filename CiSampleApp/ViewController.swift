29ac5032457e886f51b5f437f409413883688255
真田雄太 <>
2018-11-10 04:00:37 +0900

    first commit

---
+++ CiSampleApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CiSampleApp
+//
+//  Created by 真田雄太 on 2018/11/10.
+//  Copyright © 2018 yutaSanada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        label.text = "Hello Gizumo"
+    }
+
+
+}
+
